1050d4137b5e05bb1aa03a9be491b2430b574425
sjcowdery96 <sjc.cowdery@gmail.com>
2023-10-05 20:06:05 -0500

    added display.js to handle inputs

---
+++ display.js
@@ -0,0 +1,13 @@
+//this file will be used to actuall deal with the user inputs etc.
+
+//very simple input function to get us started
+function checkInput() {
+    const input = document.getElementById("input").value;
+    const integerValue = parseInt(input, 10);
+
+    if (!Number.isInteger(integerValue)) {
+        document.getElementById("output").innerHTML = `Not an Integer -- Input is: ${input}`;
+    } else {
+        document.getElementById("output").innerHTML = `Input is an integer: ${input}`;
+    }
+}
